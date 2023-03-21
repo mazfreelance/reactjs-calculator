3fddf5ad6e5353172789f69a09d25db54a565186
mazfreelance <freelance@mohdazmin.com>
2023-03-21 12:40:33 +0800

    add functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import classes from './Button.module.css'
 
-function Button({ value }) {
+function Button({ value, handler }) {
     return (
         <>
-            <div className={ value == "C" || value == "=" ? `${classes.calcBtns} ${classes.res}` : `${classes.calcBtns}` }>
+            <div className={ value == "C" || value == "=" ? `${classes.calcBtns} ${classes.res}` : `${classes.calcBtns}` } onClick={ () => handler(value)}>
                 { value }
             </div>
         </>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 import classes from "./Home.module.css";
 // import Demo from './Demo'
 
 function Home() {
+
+    const [res, setRes] = useState("")
+
     const buttons = [
         "C", "9", "/", "8", "7", "6", "*",
         "5", "4", "3", "+", "2", "1", "0",
         "-", ".", "Del", "="
     ];
 
+    const findval = () => {
+        let result = Function("return "+ res)()
+        setRes(result.toString())
+    }
+
+    const handler = (arg) => {
+        // console.log("🚀 ~ file: Home.js:22 ~ handler ~ arg:", arg)
+        if(arg == "C") setRes("")
+        else if(arg == "=") findval()
+        else if(arg == "Del") {
+            let n = res.length
+            if(n>0) setRes(res.slice(0,n-1))
+        }
+        else setRes(res.concat(arg))
+    }
+
     return (
         <div className={ classes.home }>
             <div className={ classes.inner }>
                 <div className={ classes.result }>
                     <div className={ classes.resbox }>
-
+                        { res }
                     </div>
                 </div>
                 <div className={ classes.btns }>
                     {buttons.map((element, idx) => {
-                        return <Button value={element}/>
+                        return <Button handler={handler} value={element}/>
                     })}
                 </div>
             </div>
